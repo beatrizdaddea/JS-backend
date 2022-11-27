1e709a21ad8278b31930372ee58b92279bdf0838
beatrizdaddea <beatrizchith@gmail.com>
2022-11-27 09:47:09 -0300

    Array JS

---
+++ Arrays/media.js
@@ -0,0 +1,58 @@
+// usando o for of
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+for (let nota of notas) {
+  somaDasNotas += nota;
+}
+
+const media = somaDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}.`);
+
+// usando o for each
+
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+notas.forEach(function (nota) {
+  somaDasNotas += nota;
+});
+
+const media = somaDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}.`);
+
+// usando o for geral de
+
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+for (let i = 0; i < notas.length; i++) {
+  somaDasNotas += notas[i];
+}
+
+const media = somaDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}.`);
+
+// usando o reduce para
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+const salaJava = [6, 5, 8, 9, 5, 6];
+const salaPython = [7, 3.5, 8, 9.5];
+
+function calculaMedia(notasDaSala) {
+  const somaDasNotas = notasDaSala.reduce((acc, nota) => acc + nota, 0);
+
+  const media = somaDasNotas / notasDaSala.length;
+
+  return media;
+}
+
+console.log(`A média da sala de JavaScript é ${calculaMedia(salaJS)}`);
+console.log(`A média da sala de Java é ${calculaMedia(salaJava)}`);
+console.log(`A média da sala de Python é ${calculaMedia(salaPython)}`);
+
---
+++ map.js
@@ -0,0 +1,17 @@
+// map em string 
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesPadronizados);
+
+
+// map em array 
+
+const notas = [10, 9.5, 8, 7, 6];
+
+const notasAtualizadas = notas.map((nota) => {
+  return nota + 1 >= 10 ? 10 : nota + 1;
+});
+
+console.log(notasAtualizadas);
